46e417cd8ea1d3788ae72c6e6d43d41b9b86b5fb
NataliiaChapai <natalya.chapay@gmail.com>
2021-11-18 01:46:48 +0200

    add modal for header

--- src/modal.js
+++ src/modal.js
@@ -1,14 +1,29 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+    const modal = document.querySelector('.js-modal-container');
+    const openModalBtn = document.querySelector('.js-open-modal');
+    const closeModalBtn = document.querySelector('.js-close-modal');
+  
+    const toggleModal = () => {
+      const isModalOpen =
+        openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+      openModalBtn.setAttribute('aria-expanded', !isModalOpen);
+      modal.classList.toggle('is-open-modal');
+  
+      const scrollLockMethod = !isModalOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+  
+    openModalBtn.addEventListener('click', toggleModal);
+    closeModalBtn.addEventListener('click', toggleModal);
+  
+    // Закрываем мобильное меню на более широких экранах
+    // в случае изменения ориентации устройства.
+    window.matchMedia('(min-width: 2000px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      modal.classList.remove('is-open-modal');
+      openModalBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
