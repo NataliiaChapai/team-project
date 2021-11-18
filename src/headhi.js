9a6ae17db8ecdb1a4379fd030b65f2361162432d
KateRashidova <katerashidova@gmail.com>
2021-11-18 18:09:19 +0200

    Виправляє фіксацію хедера

--- src/headhi.js
+++ src/headhi.js
@@ -1,19 +1,19 @@
-let lastScroll = 0;
-const defaultOffset = 50;
-const header = document.querySelector('.header');
+// let lastScroll = 0;
+// const defaultOffset = 50;
+// const header = document.querySelector('.header');
 
-const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('hide');
+// const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+// const containHide = () => header.classList.contains('hide');
 
-window.addEventListener('scroll', () => {
-    if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-        //scroll down
-        header.classList.add('hide');
-    }
-    else if(scrollPosition() < lastScroll && containHide()){
-        //scroll up
-        header.classList.remove('hide');
-    }
+// window.addEventListener('scroll', () => {
+//     if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+//         //scroll down
+//         header.classList.add('hide');
+//     }
+//     else if(scrollPosition() < lastScroll && containHide()){
+//         //scroll up
+//         header.classList.remove('hide');
+//     }
 
-    lastScroll = scrollPosition();
-})
+//     lastScroll = scrollPosition();
+// })
